5038bda2eb6cbef5bca382fb20f538a04ab5022e
junyoung <jy940608@naver.com>
2022-04-25 23:22:08 +0900

    feat: OAS 3.0 swagger 추가

--- config/urls.py
+++ config/urls.py
@@ -17,11 +17,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularJSONAPIView, SpectacularSwaggerView, \
+    SpectacularRedocView, SpectacularYAMLAPIView
 
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
+]
+
+urlpatterns += [
+    # Open API 자체를 조회 : json, yaml,
+    path("docs/json/", SpectacularJSONAPIView.as_view(), name="schema-json"),
+    path("docs/yaml/", SpectacularYAMLAPIView.as_view(), name="swagger-yaml"),
+    # Open API Document UI로 조회: Swagger, Redoc
+    path("docs/swagger/", SpectacularSwaggerView.as_view(url_name="schema-json"), name="swagger-ui", ),
+    path("docs/redoc/", SpectacularRedocView.as_view(url_name="schema-json"), name="redoc", ),
+
 ]
 
 if settings.DEBUG:
