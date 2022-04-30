0da20f9ae9658db20b155f6bb7340d9e0b12001d
junyoung <jy940608@naver.com>
2022-04-30 23:55:20 +0900

    feat: 상품 api swagger 적용

--- api/views/v1/products.py
+++ api/views/v1/products.py
@@ -2,12 +2,16 @@ from rest_framework import generics
 from api.models.products import Product
 from api.serializers.products import ProductSerializer
 
+from drf_spectacular.views import extend_schema
 
+
+@extend_schema(tags=['상품'], summary='숨김 상품을 제외한 제품 리스트')
 class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.visible_objects.all()
+    queryset = Product.visible_objects.all()    # 숨김 상품을 제외한 리스트
     serializer_class = ProductSerializer
 
 
+@extend_schema(tags=['상품'], summary='상품 상세 정보')
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
