019d382e7ddf5e8276127af9707ab2bc0d94b08a
junyoung <jy940608@naver.com>
2022-05-28 15:17:56 +0900

    feat: 주문 모델링 추가

---
+++ api/models/orders.py
@@ -0,0 +1,21 @@
+import uuid
+
+from django.db import models
+from core import models as core_models
+
+
+class Order(core_models.DateTimeModel):
+    order_number = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    user = models.ForeignKey('api.User', on_delete=models.CASCADE, related_name='orders')
+    # 배송지
+    # 결제수단
+    # 주문일시 (created_at)
+    # 결제금액 FK (결제수단명)
+
+
+class OrderHistory(core_models.DateTimeModel):
+    order = models.ForeignKey('Order', on_delete=models.CASCADE)
+    product = models.ForeignKey('api.Product', on_delete=models.SET_NULL, related_name='order_histories')
+    variant = models.ForeignKey('api.ProductVariant', on_delete=models.SET_NULL, related_name='order_histories')
+    quantity = models.PositiveIntegerField(default=1)
+    price = models.PositiveIntegerField()
