d2a1ae27084b732a417c7e4e307d40e18de8037f
junyoung <jy940608@naver.com>
2022-05-28 15:08:27 +0900

    feat: 장바구니 모델 추가

---
+++ api/models/carts.py
@@ -0,0 +1,9 @@
+from django.db import models
+from core import models as core_models
+
+
+class Cart(core_models.DateTimeModel):
+    user = models.OneToOneField('api.User', on_delete=models.CASCADE, related_name='cart')
+    product = models.ForeignKey('api.Product', on_delete=models.SET_NULL, related_name='carts')
+    variant = models.ForeignKey('api.ProductVariant', on_delete=models.SET_NULL, related_name='carts')
+
